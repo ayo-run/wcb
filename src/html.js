1ea1e3eee636200d15786be3ad81cfe15810a5b3
Ayo <ayo@ayco.io>
2026-08-08 10:05:14 +0200

    refactor: initial JSDocs hardening
    Signed-off-by: Ayo <ayo@ayco.io>

--- src/html.js
+++ src/html.js
@@ -1,18 +1,38 @@
 import htm from 'htm/mini'
 
 /**
+ * A vnode: plain object `html` produces, consumed by `createElement` or `patchNode`
+ * @typedef {object} VNode
+ * @property {string} type the tagname to create
+ * @property {Record<string, unknown> | null} props parsed attributes, `null` if none
+ * @property {VNodeChild[]} children the node's children
+ */
+
+/**
  *
- * @param type
- * @param props
- * @param {...any} children
+ * Anything that can be a vnode child:
+ * - another vnode
+ * - a primitive text
+ * - a nested array
+ * - null or undefined from a falsy branch
+ * @typedef {VNode | string | number | boolean | null | undefined | VNodeChild[]} VNodeChild
+ */
+
+/**
+ * Hyperscript function we bind `htm` to. Builds a single vnode.
+ * @param {string} type the tag name
+ * @param {Record<string, unknown> | null} props parsed attributes, `null` if none
+ * @param {...VNodeChild} children the node's children
+ * @returns {VNode} the vnode
  */
 function h(type, props, ...children) {
   return { type, props, children }
 }
 
 /**
- * For htm license information please see ./vendors/htm/LICENSE.txt
- * @license Apache <https://www.apache.org/licenses/LICENSE-2.0>
  * @author Jason Miller <jason@developit.ca>
+ * @license Apache-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * For htm license information please see ./vendors/htm/LICENSE.txt
  */
 export const html = htm.bind(h)
